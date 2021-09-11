8d36fd5069164e3d6a886914d09327f879af1e91
Cosmo Strozza <cstrozza@health.sdu.dk>
2021-09-11 11:40:08 +0200

    Dati modificati manualmente per avere la prima lettera di ogni indicatore maiuscola (serve per i titoli dei grafici).
    Grafico leggermente migliorato: pallini più grandi, aggiunti titolo, legenda e nota sulla fonte. Ora i pallini sono colorati in base alla ripartizione geografica.

--- shinyapp.R
+++ shinyapp.R
@@ -1,4 +1,4 @@
-# packages
+# Libraries ---------------------------------------------------------------
 library(readr)
 library(readxl)
 library(shiny)
@@ -10,6 +10,7 @@ library(stringr)
 library(forcats)
 library(magrittr)
 
+# Data manipulation -------------------------------------------------------
 # read data
 data <- read_delim("DCIS_INDDEMOG1_21072021152359210.csv",
                    ";",
@@ -21,7 +22,7 @@ data <- read_delim("DCIS_INDDEMOG1_21072021152359210.csv",
                    trim_ws = TRUE)
 
 
-# data
+# data manipulation
 data_reg <- data %>% 
   filter(str_length(ITTER107)<5,
          TIME<2020,
@@ -32,28 +33,39 @@ data_reg <- data %>%
                             "Provincia Autonoma Bolzano / Bozen"),
          !`Tipo indicatore`%in% c("saldo migratorio per altro motivo (per mille abitanti)",
          "saldo migratorio interno (per mille abitanti)",
-         "saldo migratorio totale (per mille abitanti)"))
+         "saldo migratorio totale (per mille abitanti)")) %>% 
+  mutate(Territorio = as.factor(Territorio),
+         Ripartizione = as.factor(case_when(Territorio %in% c("Piemonte","Valle d'Aosta / Vallée d'Aoste",
+                                                              "Liguria", "Lombardia") ~ "Nord-ovest",
+                                            Territorio %in% c("Trentino Alto Adige / Südtirol","Veneto",
+                                                              "Friuli-Venezia Giulia", "Emilia-Romagna") ~ "Nord-est",
+                                            Territorio %in% c("Toscana","Umbria","Marche","Lazio") ~ "Centro",
+                                            Territorio %in% c("Abruzzo","Molise","Campania","Puglia",
+                                                              "Basilicata","Calabria") ~ "Sud",
+                                            Territorio %in% c("Sicilia","Sardegna") ~ "Isole")))
 
-indicatore <- unique(data_reg$`Tipo indicatore`)
+levels(data_reg$Territorio)[levels(data_reg$Territorio) == "Valle d'Aosta / Vallée d'Aoste"] <- "Valle d'Aosta"
+levels(data_reg$Territorio)[levels(data_reg$Territorio) == "Trentino Alto Adige / Südtirol"] <- "Trentino Alto Adige"
+data_reg$Ripartizione <- factor(data_reg$Ripartizione,levels(data_reg$Ripartizione)[c(3,4,1,5,2)])
+
+indicatore <- as.factor(unique(data_reg$`Tipo indicatore`))
 names(indicatore) <- unique(data_reg$`Tipo indicatore`)
+levels(indicatore)
+indicatore <- factor(as.factor(indicatore), levels(as.factor(indicatore))[c(20,18,19,17,15,16,22,24,23,2,7,1,21,8,9,10,3,4,5,6,11,12,13,14)])
 anno <- unique(data_reg$TIME)
-
 
 # caption
 captions <- read_excel("captions.xlsx") %>% 
-  filter(!indicatori%in% c("saldo migratorio per altro motivo (per mille abitanti)",
-                                  "saldo migratorio interno (per mille abitanti)",
-                                  "saldo migratorio totale (per mille abitanti)")) %>% 
+  filter(!indicatori%in% c("Saldo migratorio per altro motivo (per mille abitanti)",
+                                  "Saldo migratorio interno (per mille abitanti)",
+                                  "Saldo migratorio totale (per mille abitanti)")) %>% 
   select(`descrizione indicatori`)
 capt <- captions$`descrizione indicatori`
 names(capt) <- names(indicatore)
 # caption <- "prova"
 
 
-
-# ui and server -----------------------------------------------------------
-
-
+# UI and Server -----------------------------------------------------------
 # Define UI for application that draws a plot
 ui <- fluidPage(#theme = shinytheme("slate"),
                 
@@ -67,7 +79,7 @@ ui <- fluidPage(#theme = shinytheme("slate"),
                                  label     = "Scegli l'indicatore",
                                  choices   = indicatore, 
                                  multiple = FALSE,
-                                 selected  = "numero medio di figli per donna",
+                                 selected  = "Speranza di vita alla nascita - totale",
                                  options = list(create = TRUE, maxItems = 1)
                                ),
                                selectizeInput( # choose the year
@@ -95,20 +107,23 @@ server <- function(input, output) {
       indic = input$indicatore
       an = input$anno
       
-      
       # plot
       data_reg %>% 
         filter(`Tipo indicatore` == indic,
-               TIME == an) %>% 
+               TIME == an) %>%
+        #mutate(`Tipo indicatore` %>% as.factor() = factor(`Tipo indicatore`, levels(`Tipo indicatore`)[23,14,12,24,15,13,2,3,1,10,11,8,9,16,17,18,19,20,21,22,4,5,6,7])) %>% 
         # arrange(desc(Value)) %>% 
         ggplot(aes(x = Value, y = reorder(Territorio,Value),
-                   color = Territorio)) +
-        geom_point(size = 1.5)+
-        labs(x = element_text("Valore"), y = element_text("Regione"))+
+                   color = Ripartizione)) +
+        geom_point(size = 3)+
+        labs(x = element_blank(),
+             y = element_blank(),
+             color = "Ripartizione\ngeografica",
+             title = element_text(paste(indic, "nelle regioni italiane. Anno", an)),
+             caption = "Fonte: Istat")+
         theme_minimal()+
         theme(text = element_text(size = 15),
-              legend.position ="none")+
-        guides(fill=guide_legend(nrow=5,byrow=TRUE))
+              legend.position = "bottom")
 
   })#, width = 800, height = 315, res = 100)
   
@@ -122,5 +137,6 @@ server <- function(input, output) {
   
 }
 
-# Run the application 
+
+# RUN THE APP -------------------------------------------------------------
 shinyApp(ui = ui, server = server)
