5041f7b9f469ccd3bb8b8a5b6430e6bdb2837e72
MMargherita <margherita.moretti@uniroma1.it>
2021-09-13 19:06:55 +0200

    modifiche captions + pagina introduttiva

---
+++ rconnect.R
@@ -0,0 +1,4 @@
+library(rsconnect)
+rsconnect::setAccountInfo(name='margherita-moretti',
+                          token='AFFF896E6F15F29B766333A82BD8E37E',
+                          secret='3kY+/Kt/4PcQtu0MB1oCh3EuZl3wWt+kRIuXYCcX')
