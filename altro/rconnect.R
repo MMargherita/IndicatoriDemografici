a141aaaa2e64e2974a8e846d7b2602cc2937af01
MMargherita <margherita.moretti@uniroma1.it>
2021-09-14 09:32:58 +0200

    last commit

--- altro/rconnect.R
+++
@@ -1,4 +0,0 @@
-library(rsconnect)
-rsconnect::setAccountInfo(name='margherita-moretti',
-                          token='AFFF896E6F15F29B766333A82BD8E37E',
-                          secret='3kY+/Kt/4PcQtu0MB1oCh3EuZl3wWt+kRIuXYCcX')
