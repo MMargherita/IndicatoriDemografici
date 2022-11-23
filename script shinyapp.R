73a60c2f1f0cc5bbc4d6fae6967e323439dce4cb
MMargherita <margherita.moretti@uniroma1.it>
2022-11-23 15:59:38 +0100

    updates with forecast

--- script shinyapp.R
+++ script shinyapp.R
@@ -1,35 +1,45 @@
-# Libraries ---------------------------------------------------------------
-library(readr)
-library(readxl)
+# Libraries -----
 library(tidyverse)
 library(dplyr)
-library(ggplot2)
-library(ggdark)
 library(shiny)
 library(shinythemes)
+library(bslib)
+library(markdown)
+library(readxl)
+library(readr)
+library(ggplot2)
+library(ggdark)
 
 
-# Data manipulation ------------------------------------------------------------
-# read data
-data <- read_delim("data/DCIS_INDDEMOG1_21072021152359210.csv",
-                   ";",
-                   escape_double = FALSE,
-                   col_types = cols(TIPO_DATO15 = col_skip(),
-                                    `Seleziona periodo` = col_skip(),
-                                    `Flag Codes` = col_skip(),
-                                    Flags = col_skip()),
-                   trim_ws = TRUE)
+# N.B. perchè alcuni intervalli non sono centrati?? es. indice di vecchiaia...
 
-# filter what we want and adjust levels
-data_reg <- data %>% 
+
+data <- read_csv("data/DCIS_INDDEMOG1_25072022184237822.csv", 
+                 col_types = cols(TIPO_DATO15 = col_skip(), 
+                                  `Seleziona periodo` = col_skip(), 
+                                  `Flag Codes` = col_skip(), Flags = col_skip())) %>% 
+  mutate(`Intervallo di previsione` = "mediana") %>% 
+  select(ITTER107,Territorio,`Tipo indicatore`,TIME,Value,`Intervallo di previsione`)
+
+previsioni <- read_csv("data/DCIS_PREVDEM1_23112022151530397.csv", 
+                       col_types = cols(TIPO_DATO15 = col_skip(), 
+                                        INT_PREV = col_skip(),`Seleziona periodo` = col_skip(),
+                                        `Flag Codes` = col_skip(), 
+                                        Flags = col_skip()))%>% 
+  rename(`Tipo indicatore`=`Tipo di indicatore demografico`)%>% 
+  dplyr::filter(!`Territorio`%in% c("Provincia Autonoma Bolzano / Bozen",
+                                    "Provincia Autonoma Trento")) %>% 
+  select(ITTER107,Territorio,`Tipo indicatore`,TIME,Value,`Intervallo di previsione`)
+
+
+
+
+# merge, filter what we want and adjust levels
+data_prev <- rbind(data,previsioni)
+
+data_prev_reg <- data_prev %>% 
   arrange(`Tipo indicatore`) %>% 
   filter(str_length(ITTER107)<5,
-         TIME<2020,
-         !Territorio %in% c("Nord","Centro","Sud", "Isole",
-                            "Italia",
-                            "Nord-ovest","Nord-est","Mezzogiorno",
-                            "Provincia Autonoma Trento",
-                            "Provincia Autonoma Bolzano / Bozen"),
          !`Tipo indicatore`%in% c("Saldo migratorio per altro motivo (per mille abitanti)",
                                   "Saldo migratorio interno (per mille abitanti)",
                                   "Saldo migratorio totale (per mille abitanti)")) %>% 
@@ -38,16 +48,26 @@ data_reg <- data %>%
                                             Territorio %in% c("Toscana","Umbria","Marche","Lazio") ~ "Centro",
                                             Territorio %in% c("Abruzzo","Molise","Campania","Puglia","Basilicata","Calabria","Sicilia","Sardegna") ~ "Sud e Isole")))
 
-levels(data_reg$Territorio)[levels(data_reg$Territorio) == "Valle d'Aosta / Vallée d'Aoste"] <- "Valle d'Aosta"
-levels(data_reg$Territorio)[levels(data_reg$Territorio) == "Trentino Alto Adige / Südtirol"] <- "Trentino Alto Adige"
-data_reg$Ripartizione <- factor(data_reg$Ripartizione,levels(data_reg$Ripartizione)[c(3,4,1,5,2)])
 
-anno <- unique(data_reg$TIME)
+# to wide format to add the CIs variable
+data_prev_reg_wide <- data_prev_reg %>% 
+  pivot_wider(names_from = `Intervallo di previsione`,
+              values_from = `Value`)
 
-indicatore <- as.factor(unique(data_reg$`Tipo indicatore`))
-names(indicatore) <- unique(data_reg$`Tipo indicatore`)
+
+levels(data_prev_reg_wide$Territorio)[levels(data_prev_reg_wide$Territorio) == "Valle d'Aosta / Vallée d'Aoste"] <- "Valle d'Aosta"
+levels(data_prev_reg_wide$Territorio)[levels(data_prev_reg_wide$Territorio) == "Trentino Alto Adige / Südtirol"] <- "Trentino Alto Adige"
+data_prev_reg_wide$Ripartizione <- factor(data_prev_reg_wide$Ripartizione,levels(data_prev_reg_wide$Ripartizione)[c(3,4,1,5,2)])
+
+anno <- unique(data_prev_reg_wide$TIME)
+
+indicatore <- as.factor(unique(data_prev_reg_wide$`Tipo indicatore`))
+names(indicatore) <- unique(data_prev_reg_wide$`Tipo indicatore`)
 levels(indicatore)
 indicatore <- sort(factor(as.factor(indicatore), levels(as.factor(indicatore))[c(17,15,16,14,12,13,19,21,20,7,2,3,8,9,10,18,1,11,4,5,6)]))
+
+
+
 
 
 # read captions
@@ -66,28 +86,33 @@ names(capt) <- names(indicatore)
 
 
 
+
+
 # UI and Server -----------------------------------------------------------
+
+# set-up for light/dark mode
+light <- bs_theme(bootswatch = "flatly")
+# dark <- bs_theme(bg = "black", fg = "white", primary = "purple")
+# dark <- bs_theme(bootswatch = "darkly")
+
 
 # ui
 ui <- navbarPage(
-  theme = shinytheme("cyborg"),
-  title = "INDICATORI DEMOGRAFICI", 
+  title = "INDICATORI DEMOGRAFICI",
+  
+  # theme = shinytheme("cyborg"),
+  theme = light, 
+  # checkboxInput("dark_mode", "Dark mode"),
+  
   tabPanel(
     "Descrizione", # cover page
-    h1("METTI LE REGIONI IN CLASSIFICA ... DEMOGRAFICAMENTE!"),
-    h4("Con questa applicazione puoi visualizzare la classifica delle regioni italiane secondo alcuni dei principali indicatori demografici, dal 2002 al 2019."),
-    h4("Scopri di più cliccando in alto < Visualizzazione interattiva > ! Una volta lì, potrai scegliere l'indicatore demografico e l'anno di interesse e ottenere la classifica delle regioni italiane. Sotto ogni grafico è riportata la descrizione dell'indicatore selezionato."),
-    h3(""),
-    h3(""),
-    h5("Realizzato per il festival della statistica e della demografia StatisticAll 2021, a cura di:"),
-    h6("Margherita Moretti",tags$sup(1),", Cosmo Strozza",tags$sup(2),"e Cecilia Fortunato",tags$sup(1)," del gruppo Demografica...Mente!"),
-    h6(tags$sup(1),"Dipartimento di Scienze Statistiche, Sapienza Università di Roma"),
-    h6(tags$sup(2),"Interdisciplinary Centre on Population Dynamics, University of Southern Denmark")
-    ), 
+    withMathJax((includeMarkdown("Cover_Page.Rmd")))
+  ), 
+  
+  
   tabPanel(
     "Visualizzazione interattiva", # page with interactive plot
-    titlePanel(
-      h3("Scopri alcuni dei principali indicatori demografici")),
+    withMathJax((includeMarkdown("Figure.Rmd"))),
     sidebarLayout(
       sidebarPanel(width = 4,
                    selectizeInput( # select indicator
@@ -116,25 +141,34 @@ ui <- navbarPage(
 )
 
 
+
 # server 
-server <- function(input, output) {
+server <- function(input, output){#, session) {
+  
+  # observe(session$setCurrentTheme(
+  #   if (isTRUE(input$dark_mode)) dark else light
+  # ))
   
   output$plot <- renderPlot({ # plot
     indic = input$indicatore
     an = input$anno
-
-    data_reg %>% 
+    
+    data_prev_reg_wide %>% 
       filter(`Tipo indicatore` == indic,
              TIME == an) %>%
-      ggplot(aes(x = Value, y = reorder(Territorio,Value),
+      ggplot(aes(x = mediana, y = reorder(Territorio,mediana),
                  color = fct_rev(Ripartizione))) +
       geom_point(size = 3.5)+
+      geom_errorbarh(aes(xmin=`limite inferiore 90%`,
+                         xmax=`limite superiore 90%`,
+                         color = fct_rev(Ripartizione)))+
       labs(x = element_blank(),
            y = element_blank(),
            color = "Ripartizione geografica",
            title = str_wrap(element_text(paste(indic, "nelle regioni italiane. Anno",an)), 80),
            caption = "Fonte dati: Istat\nA cura di: M. Moretti, C. Strozza, C. Fortunato (Demografica...Mente!)")+
-      dark_theme_minimal()+
+      # dark_theme_minimal()+
+      theme_minimal()+
       theme(text = element_text(size = 13),
             axis.text.y = element_text(size = 10),
             legend.position = "bottom")+
@@ -147,13 +181,10 @@ server <- function(input, output) {
     capt[input$indicatore] # captions
   })
   
-}
+  
+}  
 
 
 # Run the app ------------------------------------------------------------------
 shinyApp(ui = ui, server = server)
 
-
-  
-    
-    
